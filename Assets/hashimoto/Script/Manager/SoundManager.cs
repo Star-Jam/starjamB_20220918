951ad4266f51f62b8aa6500e30c10d7220495a48
rikuriku0402 <89211617+rikuriku0402@users.noreply.github.com>
2022-09-18 12:52:41 +0900

    シングルトンを追加 サウンドマネージャーを作成

---
+++ Assets/hashimoto/Script/Manager/SoundManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : SingletonMonoBehaviour<SoundManager>
+{
+    [SerializeField]
+    [Header("タイトル")]
+    AudioClip _titleBGM;
+
+    [SerializeField]
+    [Header("ゲーム")]
+    AudioClip _gameBGM;
+
+    [SerializeField]
+    [Header("リザルト")]
+    AudioClip _resultBGM;
+
+    [SerializeField]
+    [Header("プレイヤーが死んだときの音")]
+    AudioClip _deathSFX;
+
+    [SerializeField]
+    [Header("アイテムに触れたときの音")]
+    AudioClip _itemSFX;
+
+    AudioSource _audioSource;
+
+    const string TITLE_SCENE_NAME = "TitleScene";
+    const string GAME_SCENE_NAME = "GameScene";
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (TryGetComponent(out _audioSource))
+        {
+            Debug.Log("オーディオソースが参照できた");
+        }
+
+        FirstBGM();
+    }
+
+    void FirstBGM()
+    {
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case TITLE_SCENE_NAME:
+                _audioSource.clip = _titleBGM;
+                break;
+
+            case GAME_SCENE_NAME:
+                _audioSource.clip = _gameBGM;
+                break;
+        }
+
+        _audioSource.Play();
+    }
+
+    public void PlayBGM(BGMType type)
+    {
+        switch (type)
+        {
+            case BGMType.Title:
+                _audioSource.clip = _titleBGM;
+                break;
+
+            case BGMType.Game:
+                _audioSource.clip = _gameBGM;
+                break;
+
+            case BGMType.Result:
+                _audioSource.clip = _resultBGM;
+                break;
+        }
+
+        _audioSource.Play();
+    }
+
+    public void PlaySFX(SFXType type)
+    {
+        switch (type)
+        {
+            case SFXType.Death:
+                _audioSource.PlayOneShot(_deathSFX);
+                break;
+
+            case SFXType.Item:
+                _audioSource.PlayOneShot(_itemSFX);
+                break;
+        }
+    }
+}
